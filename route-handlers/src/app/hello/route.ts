5fdeda14d24b5d2e32781453e864c08745840403
Alecto <fomenkoandrey1978@gmail.com>
2024-07-13 10:28:03 +0300

    route-handlers - basic usage

---
+++ route-handlers/src/app/hello/route.ts
@@ -0,0 +1,19 @@
+export async function GET() {
+  return new Response('GET! Hello, World!')
+}
+
+export async function POST() {
+  return new Response('POST! Hello, World!')
+}
+
+export async function PUT() {
+  return new Response('PUT! Hello, World!')
+}
+
+export async function PATCH() {
+  return new Response('PATCH! Hello, World!')
+}
+
+export async function DELETE() {
+  return new Response('DELETE! Hello, World!')
+}
---
+++ route-handlers/src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h1 className="text-9xl text-center my-24">404</h1>
+}
